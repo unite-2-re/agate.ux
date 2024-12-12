2ba8ba46cb33a9a2e66218f83b9f2501f6d08f5b
u2re-dev <conway24b@gmail.com>
2024-12-12 21:45:16 +0700

    WIP orient-box

---
+++ src/ts/wcomp/OrientBox.ts
@@ -0,0 +1,82 @@
+//
+import {zoomOf} from "../sw/Zoom";
+
+// @ts-ignore
+import styles from "./OrientBox.scss?inline&compress";
+
+// @ts-ignore
+import html from "./OrientBox.html?raw";
+
+//
+const preInit = URL.createObjectURL(new Blob([styles], {type: "text/css"}));
+
+//
+export default class UIOrientBox extends HTMLElement {
+    static observedAttributes = ["orient", "zoom"];
+
+    //
+    #themeStyle?: HTMLStyleElement;
+    #initialized: boolean = false;
+
+    //
+    get orient() { return parseInt(this.getAttribute("orient") || "0"); };
+    set orient(ox) { this.setAttribute("orient", (ox || 0).toFixed(0)); };
+
+    //
+    get zoom() { return parseFloat(this.getAttribute("zoom") || "1") || 1; };
+    set zoom(ox) { this.setAttribute("zoom", (ox || 1).toFixed(0)); };
+
+    //
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({mode: "open"});
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(html, "text/html");
+
+        // @ts-ignore
+        const THEME_URL = "/externals/core/theme.js";
+        import(/* @vite-ignore */ "" + `${THEME_URL}`).then((module)=>{
+            // @ts-ignore
+            this.#themeStyle = module?.default?.(this.shadowRoot);
+            if (this.#themeStyle) { this.shadowRoot?.appendChild?.(this.#themeStyle); }
+        }).catch(console.warn.bind(console));
+
+        //
+        dom.querySelector("template")?.content?.childNodes.forEach(cp => {
+            shadowRoot.appendChild(cp.cloneNode(true));
+        });
+
+        //
+        const style = document.createElement("style");
+        style.innerHTML = `@import url("${preInit}");`;
+
+        //
+        shadowRoot.appendChild(style);
+        this.style.setProperty("--orient", this.getAttribute("orient"));
+        this.style.setProperty("--zoom", this.getAttribute("zoom"));
+    }
+
+    //
+    #initialize() {
+        if (this.#initialized) return this;
+        const shadowRoot = this.shadowRoot;
+        this.#initialized = true;
+        this.style.setProperty("--orient", this.getAttribute("orient"));
+        this.style.setProperty("--zoom", this.getAttribute("zoom"));
+        return this;
+    }
+
+    //
+    connectedCallback() {
+        this.#initialize();
+    }
+
+    //
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == "orient") { this.style.setProperty("--orient", newValue); };
+        if (name == "zoom") { this.style.setProperty("--zoom", newValue); };
+    }
+}
+
+//
+customElements.define("ui-orientbox", UIOrientBox);
